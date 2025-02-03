1b96040ecaba36948f97032ebefa233c0f56e8f6
Manjesh R V <manjeshrv592@gmail.com>
2025-02-03 14:21:05 +0530

    Added text splash screen

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -1,20 +1,21 @@
 "use client";
 import React from "react";
-import Lottie from "lottie-react";
-import splashScreenData from "@/animations/splash-screen";
-// import { useRegions } from "@/contexts/RegionContext";
+// import Lottie from "lottie-react";
+// import splashScreenData from "@/animations/splash-screen";
+import { useRegions } from "@/contexts/RegionContext";
 
 const SplashScreen = () => {
-  // const { regionIsLoading } = useRegions();
+  const { regionIsLoading } = useRegions();
 
-  // if (!regionIsLoading) {
-  //   return null;
-  // }
+  if (!regionIsLoading) {
+    return null;
+  }
 
   return (
     <div className="fixed left-0 top-0 z-[1000] flex size-full items-center justify-center bg-white">
       <div className="max-w-screen-c-md">
-        <Lottie animationData={splashScreenData} loop={true} autoplay={true} />
+        <h1 className="text-4xl font-bold text-brand-blue">Splash screen</h1>
+        {/* <Lottie animationData={splashScreenData} loop={true} autoplay={true} /> */}
       </div>
     </div>
   );
