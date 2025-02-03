2384378f9ed529923be6947bacaf3a0f9e8c7732
Manjesh R V <manjeshrv592@gmail.com>
2025-02-03 14:18:00 +0530

    Removed use rgions from splash screen

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -2,14 +2,14 @@
 import React from "react";
 import Lottie from "lottie-react";
 import splashScreenData from "@/animations/splash-screen";
-import { useRegions } from "@/contexts/RegionContext";
+// import { useRegions } from "@/contexts/RegionContext";
 
 const SplashScreen = () => {
-  const { regionIsLoading } = useRegions();
+  // const { regionIsLoading } = useRegions();
 
-  if (!regionIsLoading) {
-    return null;
-  }
+  // if (!regionIsLoading) {
+  //   return null;
+  // }
 
   return (
     <div className="fixed left-0 top-0 z-[1000] flex size-full items-center justify-center bg-white">
